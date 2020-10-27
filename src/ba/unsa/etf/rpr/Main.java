8471cec3b94d93da15fc4866c7cebc8f079e86c4
aprljaca1 <aprljaca1@etf.unsa.ba>
2020-10-27 16:24:13 +0100

    Main

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,15 @@
+package ba.unsa.etf.rpr;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        public int brojac=0;
+        public int zbir=0;
+        Scanner ulaz = new Scanner(System.in);
+        System.out.println("Unesite n: ");
+        int n = ulaz.nextInt();
+
+    }
+}
